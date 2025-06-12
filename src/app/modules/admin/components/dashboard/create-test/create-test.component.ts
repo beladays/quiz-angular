ff1f124816ca48c53912837dab50cbf42f132af2
Isabela <isabeladays63@gmail.com>
2025-06-12 01:27:43 -0300

    validação da tela de criar quiz

--- src/app/modules/admin/components/dashboard/create-test/create-test.component.ts
+++ src/app/modules/admin/components/dashboard/create-test/create-test.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../../shared/auth/signup/shared/shared.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { AdminService } from '../../../services/admin.service';
 
 @Component({
   selector: 'app-create-test',
@@ -11,7 +14,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './create-test.component.css'
 })
 export class CreateTestComponent {
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder,
+    private deviceService: AdminService,
+    private notifications: NzNotificationService,
+    private router: Router,
+
+  ){}
 
   testForm! : FormGroup;
 
@@ -22,4 +30,29 @@ export class CreateTestComponent {
       time: [null, [Validators.required]],
     })
   }
+
+    submitForm(){
+      if(this.testForm.valid){
+        this.deviceService.createTest(this.testForm.value).subscribe(res=>{
+          this.notifications
+            .success(
+              'SUCESS',
+              'Quiz criado com sucesso.',
+              {nzDuration: 5000}
+            );
+            this.router.navigateByUrl("/admin/dashboard")
+      
+        }, error=> {
+          this.notifications
+            .error(
+              'ERROR',
+              `${error.error}`,
+              {nzDuration: 5000}
+            )
+        }
+      )
+      }
+    }
+
+
 }
