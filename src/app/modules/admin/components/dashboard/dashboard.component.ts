6e56a9eacb8323cf5f802a63e9c4a7ed824dc730
Isabela <isabeladays63@gmail.com>
2025-06-12 02:02:18 -0300

    Criado tela de dashboard o admin

--- src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/auth/signup/shared/shared.module';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    SharedModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  tests= [];
+
+  constructor(private notification: NzNotificationService,
+    private testService: AdminService
+  ){}
+
+  ngOnInit(){
+    this.getAllTests();
+  }
+
+  getAllTests(){
+    this.testService.getAllTests().subscribe(res=>{
+      this.tests = res;
+    }, error=>{
+      this.notification
+      .error(
+        'ERROR',
+        `Algo deu errado. Tente novamente`,
+        {nzDuration: 5000}
+      )
+    }
+  )
+  }
+  
 
 }
