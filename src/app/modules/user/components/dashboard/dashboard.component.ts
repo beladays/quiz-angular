1df73433135e48ca7a2a804d69633150dff4b63a
Isabela <isabeladays63@gmail.com>
2025-06-13 00:07:26 -0300

    Tela de dashboard do usuario criada

--- src/app/modules/user/components/dashboard/dashboard.component.ts
+++ src/app/modules/user/components/dashboard/dashboard.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/auth/signup/shared/shared.module';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { TestService } from '../../services/test.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    SharedModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+    tests= [];
+
+    constructor(private notification: NzNotificationService,
+      private testService: TestService
+    ){}
+
+    ngOnInit(){
+    this.getAllTests();
+  }
+
+  getAllTests(){
+    this.testService.getAllTests().subscribe(res=>{
+      this.tests = res;
+    }, error=>{
+      this.notification
+      .error(
+        'ERROR',
+        `Algo deu errado. Tente novamente`,
+        {nzDuration: 5000}
+      )
+    }
+  )
+  }
+  
+
 }
+
+
