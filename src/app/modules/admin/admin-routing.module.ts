e3b36c67ed8e6be2ed3190a65e149818fc6645df
Isabela <isabeladays63@gmail.com>
2025-06-26 14:11:46 -0300

    retirada de rotas n utilizadas

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CreateTestComponent } from './components/dashboard/create-test/create-test.component';
 import { ViewTestComponent } from './components/view-test/view-test.component';
-import { ViewTestResultsComponent } from './components/view-test-results/view-test-results.component';
 import { EditQuizComponent } from './components/edit-quiz/edit-quiz.component';
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
    {path: 'create-test', component: CreateTestComponent},
    {path: 'view-test/:id', component: ViewTestComponent},
-   {path: 'view-test-results', component: ViewTestResultsComponent},
    { path: 'edit-quiz/:id', component: EditQuizComponent }
 
 
